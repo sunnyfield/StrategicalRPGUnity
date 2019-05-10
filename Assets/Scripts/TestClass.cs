1adbb0cfe979930f3aac295f0d7915c77a134cd3
Safonov Alexander <saf.alex1@gmail.com>
2019-05-10 17:19:33 +0300

    Редактирование тестового скрипта.
    Должен выводить в консоль сообщение.

--- Assets/Scripts/TestClass.cs
+++ Assets/Scripts/TestClass.cs
@@ -7,7 +7,7 @@ public class TestClass : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("Hello Git");
     }
 
     // Update is called once per frame
